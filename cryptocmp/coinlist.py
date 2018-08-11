297fc51be40a3e7e52532bd46d05be5b68d6f687
RoyPy <bogush.vano@gmail.com>
2018-08-11 21:06:46 +0700

    Add api call wrapper for coinlist

---
+++ cryptocmp/coinlist.py
@@ -0,0 +1,37 @@
+from cryptocmp import decorators
+
+
+@decorators.extract_data
+@decorators.response_error_raise
+@decorators.get('data/all/coinlist')
+def get(
+        built_on=None,
+        extra_params=None,
+        sign=None,
+):
+    """
+    Returns all the coins that CryptoCompare has added to the website.
+
+    This is not the full list of coins in the system, it is just the list of
+    coins CryptoCompare has done some research on.
+
+    :param built_on:
+        The platform that the token is built on [Max character length: 10]
+    :param extra_params:
+        The name of your application
+        (recommended to send it) [Max character length: 2000]
+    :param sign:
+        If set to true, the server will sign the requests
+        (by default CryptoCompare doesn't sign them),
+        this is useful for usage in smart contracts
+    :return:
+        List of available coins in CryptoCompare.
+    """
+
+    # use limit-1 because it seems api interprets it as the last index
+    # even though they described it as "The number of data points to return"
+    return {
+        'builtOn': built_on,
+        'extraParams': extra_params,
+        'sign': sign,
+    }
