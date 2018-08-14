7f7fcaa93d71fcc152cd6e7a27d70ce0052a6852
RoyPy <bogush.vano@gmail.com>
2018-08-14 10:13:38 +0700

    Patch project with files generated by Cookiecutter

--- setup.py
+++ setup.py
@@ -1,21 +1,50 @@
-import setuptools
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+"""The setup script."""
 
-setuptools.setup(
-    name="cryptocmp",
-    version="0.0.1",
-    author="OkThought",
-    author_email="bogush.vano@gmail.com",
-    description="Python3 Wrapper for CryptoCompare's public API",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/OkThought/cryptocmp",
-    packages=setuptools.find_packages(),
-    classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ),
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = []
+
+setup_requirements = []
+
+test_requirements = []
+
+setup(
+    author="Ivan Bogush",
+    author_email='bogush.vano@gmail.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="Python3 Wrapper for CryptoCompare public API",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='cryptocmp',
+    name='cryptocmp',
+    packages=find_packages(include=['cryptocmp']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/OkThought/cryptocmp',
+    version='0.0.1',
+    zip_safe=False,
 )
