ab74950d24287a57c23013e74d1f7b3f644e7f3c
RoyPy <bogush.vano@gmail.com>
2018-08-14 18:39:47 +0700

    Remove history from long description

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 requirements = [
     'requests',
 ]
@@ -37,7 +34,7 @@ setup(
     description="Python3 Wrapper for CryptoCompare public API",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     long_description_content_type='text/x-rst',
     include_package_data=True,
     keywords='cryptocmp',
