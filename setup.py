e86c11a58f0ef4dcae55d36dd6a8d438a9924fd6
RoyPy <bogush.vano@gmail.com>
2018-08-15 12:33:34 +0700

    Remove development status from pypi package classifiers

--- setup.py
+++ setup.py
@@ -19,11 +19,10 @@ setup(
     author="Ivan Bogush",
     author_email='bogush.vano@gmail.com',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
