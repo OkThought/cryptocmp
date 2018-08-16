8ef045e80f3c070f4cd98276d4054995d3e4e265
RoyPy <bogush.vano@gmail.com>
2018-08-16 14:27:09 +0700

    Implement CoinPair.price_history()

--- cryptocmp/coin_pair.py
+++ cryptocmp/coin_pair.py
@@ -1,5 +1,36 @@
+from warnings import warn
+
 import cryptocmp.api.price.single
 from cryptocmp.coin import Coin
+import datetime
+
+
+MINUTE = datetime.timedelta(minutes=1)
+HOUR = datetime.timedelta(hours=1)
+DAY = datetime.timedelta(days=1)
+
+_time_units = {
+    'minute': MINUTE,
+    'minutely': MINUTE,
+    'hour': HOUR,
+    'hourly': HOUR,
+    'day': DAY,
+    'dayly': DAY,
+}
+
+
+def _price_history_getter(time_unit):
+    getter = None
+    if time_unit is DAY:
+        import cryptocmp.api.historical.price.days
+        getter = cryptocmp.api.historical.price.days.get
+    elif time_unit is HOUR:
+        import cryptocmp.api.historical.price.hours
+        getter = cryptocmp.api.historical.price.hours.get
+    elif time_unit is MINUTE:
+        import cryptocmp.api.historical.price.minutes
+        getter = cryptocmp.api.historical.price.minutes.get
+    return getter
 
 
 def _coin(coin_or_symbol):
@@ -55,6 +86,57 @@ class CoinPair:
             sign=sign,
         )
         return price_dict[self.first.symbol]
+
+    def price_history(self, time_from=None, time_to=None, time_unit=None,
+                      limit=None, exchange=None, extra_params=None, sign=None):
+        aggregate = None
+        if time_unit is None:
+            time_unit = DAY
+        elif isinstance(time_unit, datetime.timedelta):
+            if time_unit < MINUTE:
+                warn('Time unit less than 1 minute is currently not supported.'
+                     'Using the minimum time unit of 1 minute instead.')
+                time_unit = MINUTE
+            elif time_unit < HOUR:
+                aggregate=time_unit.total_seconds() / 60
+                time_unit = MINUTE
+            elif time_unit < DAY:
+                aggregate=time_unit.total_seconds() / 3600
+                time_unit = HOUR
+            else:
+                aggregate=time_unit.days
+                time_unit = DAY
+        elif isinstance(time_unit, str):
+            try:
+                time_unit = _time_units[time_unit]
+            except KeyError:
+                raise ValueError('Unknown time_unit: %s' % time_unit)
+        else:
+            raise TypeError('Unsupported type of time_unit: %s' %
+                            type(time_unit))
+
+        if time_to is None:
+            time_to = datetime.datetime.now()
+
+        if time_from is not None:
+            if limit is not None:
+                raise RuntimeError('time_from and limit must not be specified'
+                                   'together')
+            time_period = time_to - time_from
+            limit = time_period // time_unit
+
+        price_history_getter = _price_history_getter(time_unit)
+
+        return price_history_getter(
+            coin=self.first.symbol,
+            in_coin=self.second.symbol,
+            to_timestamp=time_to.timestamp(),
+            limit=limit,
+            exchange=exchange,
+            aggregate=aggregate,
+            extra_params=extra_params,
+            sign=sign,
+        )
 
     def __str__(self):
         return '%s/%s' % (self.first, self.second)
